768a571cefa349250f3469c949ea1ea817bd8ff7
suliman <suliman@suliman-Linux>
2020-01-07 00:12:48 +0100

    cons

--- src/com/farzat/javaBasics/MainClass.java
+++ src/com/farzat/javaBasics/MainClass.java
@@ -6,50 +6,77 @@ package com.farzat.javaBasics;
 
 import com.farzat.javaBasics.calculator.MathOperator;
 import com.farzat.javaBasics.calculator.SelectOperator;
+import com.farzat.javaBasics.oop.ConstructorMethods;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 
-			StringClass clsStr = new StringClass();
-			DateClass clsDat = new DateClass();
-			ErrorsClass clsErr = new ErrorsClass();
-			ArraysClass clsArr = new ArraysClass();
+		// please active this row to run javaBasics Methods
+		// basicsMain();
 			
-			
-			
-			clsStr.stringEquals();
-			clsStr.StringChar();
-			clsStr.stringBuilder();
-			clsStr.stringMethods();
-			clsStr.stringScanner();
-			
-			clsDat.dateFormat();
-			clsDat.dateToString();
-			clsDat.stringToDate();
-			
-			try {
-				clsErr.thrwingError();
-			} catch (ArrayIndexOutOfBoundsException e) {
-				System.out.println("there is an error !! out of bounds ");
-			}
-			System.out.println("\n------------throwing error-------------\n");
-			clsErr.urlError();
-			clsErr.uriError();
-			
-			clsArr.simpleArray();
-			clsArr.fixArray();
-			clsArr.dynamic2dArray();
-			clsArr.fillInDynamic2dArray();
-			clsArr.listArray();
-			clsArr.hashMapArray();
-			clsArr.listIteratorsArray();
-			clsArr.iteratorsArray();
-			
-			
-			int operNum = MathOperator.insertOperator();
-			SelectOperator.operators(operNum);
+		// please active this row to run calculator Methods
+		// calculator();
+		
+
+		ConstructorMethods suliman1 = new ConstructorMethods("Suliman", "Farzat");
+		ConstructorMethods suliman2 = new ConstructorMethods("Kasem", "Farzat", 300);
+		suliman1.show();
+		suliman2.show();
 
 	}
+
+
+	
+	
+	
+	
+	
+
+	// run calculator Methods
+	private static void calculator() {
+		int operNum = MathOperator.insertOperator();
+		SelectOperator.operators(operNum);
+	}
+
+	
+	
+	// run javaBasics Methods
+	private static void basicsMain() {
+		StringClass clsStr = new StringClass();
+		DateClass clsDat = new DateClass();
+		ErrorsClass clsErr = new ErrorsClass();
+		ArraysClass clsArr = new ArraysClass();
+		
+				
+		clsStr.stringEquals();
+		clsStr.StringChar();
+		clsStr.stringBuilder();
+		clsStr.stringMethods();
+		clsStr.stringScanner();
+		
+		clsDat.dateFormat();
+		clsDat.dateToString();
+		clsDat.stringToDate();
+		
+		try {
+			clsErr.thrwingError();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("there is an error !! out of bounds ");
+		}
+		System.out.println("\n------------throwing error-------------\n");
+		clsErr.urlError();
+		clsErr.uriError();
+		
+		clsArr.simpleArray();
+		clsArr.fixArray();
+		clsArr.dynamic2dArray();
+		clsArr.fillInDynamic2dArray();
+		clsArr.listArray();
+		clsArr.hashMapArray();
+		clsArr.listIteratorsArray();
+		clsArr.iteratorsArray();
+	}
+	
 
 }
---
+++ src/com/farzat/javaBasics/oop/ConstructorMethods.java
@@ -0,0 +1,31 @@
+package com.farzat.javaBasics.oop;
+
+public class ConstructorMethods {
+	
+	String first_name;
+	String last_name;
+	int salary;
+	
+	
+	
+	public ConstructorMethods(String s1, String s2) {
+		first_name = s1;
+		last_name = s2;		
+	}
+
+
+
+	public ConstructorMethods(String s3, String s4, int n) {
+		first_name = s3;
+		last_name = s4;
+		salary = n;
+	}
+
+	public void show() {
+		System.out.println("\n------------ConstructorMethods------------\n");
+		System.out.println( "First Name: " + first_name + 
+							"\nLast Name: " + last_name + 
+							"\nSalary: " + salary );
+	}
+	
+}
